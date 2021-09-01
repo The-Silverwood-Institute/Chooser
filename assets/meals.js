a4bde0dca6cb9661a0b5e6670a10d80859a01375
Kit <kittsville@googlemail.com>
2021-09-01 16:21:57 +0100

    Add meals list

---
+++ assets/meals.js
@@ -0,0 +1,404 @@
+const meals = {
+  "Broccoli and Stilton soup": [
+    "vegetarian",
+    "soup",
+    "scales",
+    "freezes",
+    "better-next-day",
+    "slow"
+  ],
+  "Mushroom Soup": [
+    "vegan",
+    "soup",
+    "slow",
+    "scales",
+    "freezes"
+  ],
+  "Avocado and coconut soup": [
+    "vegan",
+    "soup",
+    "scales",
+    "freezes"
+  ],
+  "Spiced Parsnip & Apple Soup": [
+    "soup",
+    "scales",
+    "vegan-ish",
+    "spicy",
+    "freezes"
+  ],
+  "Spicy butternut and coconut soup": [
+    "soup",
+    "scales",
+    "vegan",
+    "freezes"
+  ],
+  "Harira Soup": [
+    "soup",
+    "scales",
+    "vegan",
+    "freezes"
+  ],
+  "Blue cheese gnocchi": [
+    "stodgy",
+    "quick",
+    "vegetarian-ish"
+  ],
+  "Stir fry": [
+    "quick",
+    "vegetarian"
+  ],
+  "Mushroom risotto": [
+    "vegetarian"
+  ],
+  "Beetroot risotto": [
+    "vegetarian",
+    "slow"
+  ],
+  "Butternut squash risotto": [
+    "vegan"
+  ],
+  "Tomato sauce": [
+    "freezes",
+    "scales",
+    "slow",
+    "better-next-day"
+  ],
+  "Roasted vegetable lasagne": [
+    "slow",
+    "effort",
+    "vegetarian",
+    "scales"
+  ],
+  "Macaroni": [
+    "scales",
+    "vegetarian"
+  ],
+  "Salmon pasta": [
+    "pescatarian"
+  ],
+  "Sweet chilli feta pasta": [
+    "quick",
+    "vegetarian"
+  ],
+  "Lemon pine nut pasta": [
+    "quick",
+    "vegetarian"
+  ],
+  "Mozzarella and spinach pancakes": [
+    "effort",
+    "vegetarian"
+  ],
+  "Melty mushroom wellingtons": [
+    "slow",
+    "vegetarian",
+    "stodge"
+  ],
+  "Courgette and spinach pasties": [
+    "slow",
+    "vegetarian",
+    "stodge"
+  ],
+  "Mushroom stroganoff": [
+    "vegetarian",
+    "scales"
+  ],
+  "Mushroom flan": [
+    "stodge",
+    "slow",
+    "vegetarian"
+  ],
+  "Broccoli & salmon flan": [
+    "stodge",
+    "slow",
+    "pescatarian"
+  ],
+  "Cheese and olive tarts": [
+    "vegetarian"
+  ],
+  "Chilli con Carne": [
+    "vegetarian-ish",
+    "freezes",
+    "better-next-day",
+    "scales"
+  ],
+  "Potato gratin": [
+    "vegetarian",
+    "slow",
+    "effort",
+    "stodge",
+    "scales"
+  ],
+  "Fettucine with Dolcelatte and Spinach": [
+    "vegetarian",
+    "quick"
+  ],
+  "Sweetcorn and spinach polenta": [
+    "vegetarian-ish",
+    "quick",
+    "scales"
+  ],
+  "Ricotta spinach pitas": [
+    "quick",
+    "vegetarian"
+  ],
+  "Tuna and rice peppers": [
+    "pescatarian",
+    "slow"
+  ],
+  "Penne with Walnut sauce": [
+    "quick",
+    "vegetarian"
+  ],
+  "Dahl": [
+    "vegan",
+    "scales"
+  ],
+  "Cod with Lentils": [
+    "quick",
+    "pescatarian"
+  ],
+  "Kashtouri  (Lentils, Rice and Pasta)": [
+    "vegan",
+    "scales"
+  ],
+  "Aubergine curry": [
+    "vegan",
+    "scales",
+    "slow"
+  ],
+  "Lentil Shepherd's Pie": [
+    "vegetarian",
+    "slow",
+    "effort",
+    "scales"
+  ],
+  "Roast Nut Omelette": [
+    "quick",
+    "vegetarian"
+  ],
+  "Baked Rigatoni with Aubergine": [
+    "vegetarian",
+    "slow",
+    "scales"
+  ],
+  "Lentil & Spinach Stew": [
+    "vegan",
+    "scales"
+  ],
+  "Mexican Polenta Pie": [
+    "vegetarian",
+    "slow",
+    "scales"
+  ],
+  "Lentil & Vegetable Pilaf": [
+    "vegan",
+    "scales"
+  ],
+  "Spicy Broccoli & Cauliflower": [
+    "vegetarian",
+    "quick",
+    "scales"
+  ],
+  "Tuna in tomato sauce": [
+    "pescatarian"
+  ],
+  "Cod in tomato sauce": [
+    "pescatarian"
+  ],
+  "Aubergine & Mozzarella Bake": [
+    "vegetarian",
+    "slow"
+  ],
+  "Aubergine & Halloumi Lasagne": [
+    "slow",
+    "vegetarian",
+    "scales"
+  ],
+  "Bean & Lentil Lasagne": [
+    "slow",
+    "effort",
+    "scales",
+    "vegetarian"
+  ],
+  "Lentil & Mint Patties": [
+    "vegetarian",
+    "effort",
+    "slow"
+  ],
+  "Carrot & Coriander Burgers": [
+    "vegetarian",
+    "slow",
+    "effort"
+  ],
+  "Parsnip and Butter Bean Crumble": [
+    "vegetarian",
+    "slow",
+    "effort",
+    "scales"
+  ],
+  "Pasta & Pesto": [
+    "vegetarian-ish",
+    "quick",
+    "scales"
+  ],
+  "Gnocchi & Tomato Bake": [
+    "vegetarian",
+    "scales"
+  ],
+  "Kedgeree": [
+    "pescatarian",
+    "scales"
+  ],
+  "Venetian Style Pasta": [
+    "quick",
+    "vegan",
+    "scales"
+  ],
+  "Egg & Mozzarella Toasts": [
+    "vegetarian",
+    "quick"
+  ],
+  "Vegetable Hot Pot": [
+    "vegetarian",
+    "slow",
+    "effort"
+  ],
+  "N Bean Chilli": [
+    "vegan",
+    "freezes",
+    "better-next-day",
+    "slow",
+    "scales"
+  ],
+  "Roast vegetable risotto": [
+    "vegetarian",
+    "slow",
+    "scales"
+  ],
+  "Mushroom and parsnip rösti pie": [
+    "vegetarian",
+    "slow",
+    "effort"
+  ],
+  "Creamy cauliflower cheese with walnuts": [
+    "vegetarian",
+    "quick"
+  ],
+  "Mushroom Lasagne": [
+    "vegetarian",
+    "slow",
+    "stodge"
+  ],
+  "Seafood Lasagne": [
+    "pescatarian",
+    "slow",
+    "stodge"
+  ],
+  "Spanakopita (Spinach/feta pastry)": [
+    "vegetarian",
+    "slow"
+  ],
+  "Saag paneer": [
+    "vegetarian"
+  ],
+  "Sweet Potato, Peanut Butter and Coconut Curry": [
+    "vegetarian"
+  ],
+  "Vegetable Primavera (baby vegetables, filled pasta and mustard)": [
+    "vegetarian",
+    "quick"
+  ],
+  "Russian Mushroom Julienne": [
+    "vegetarian",
+    "stodge"
+  ],
+  "Fishcakes": [
+    "pescatarian"
+  ],
+  "Grilled aubergine": [
+    "quick",
+    "vegan"
+  ],
+  "Paneer jalfrezi": [
+    "vegetarian",
+    "quick"
+  ],
+  "Cod and cheese stock": [
+    "pescatarian",
+    "vegan"
+  ],
+  "Smokey fish curry": [
+    "pescatarian"
+  ],
+  "Tofu and cashew nut stir fry": [
+    "vegan",
+    "quick"
+  ],
+  "Pepper and goats cheese tart": [
+    "vegetarian",
+    "slow",
+    "stodge"
+  ],
+  "Courgette and broccoli pasta": [
+    "quick",
+    "vegetarian"
+  ],
+  "Cheese scones": [
+    "vegetarian",
+    "effort",
+    "stodge"
+  ],
+  "Pizza": [
+    "effort",
+    "slow",
+    "vegetarian",
+    "stodge"
+  ],
+  "Haggis": [
+    "vegetarian",
+    "slow",
+    "effort",
+    "scales",
+    "stodge"
+  ],
+  "Kidney Bean & Vegetable gratin": [
+    "vegetarian-ish",
+    "scales"
+  ],
+  "Roast veg and chickpeas tomato sauce": [
+    "vegan",
+    "freezes",
+    "slow",
+    "scales"
+  ],
+  "Roast beetroot dahl": [
+    "vegan"
+  ],
+  "Roast vegetable Moroccan tagine": [
+    "vegan",
+    "freezes",
+    "scales"
+  ],
+  "Tofu Katsu Curry": [
+    "vegan"
+  ],
+  "Coconut & egg curry": [
+    "vegetarian"
+  ],
+  "Seitan Tagine": [
+    "vegan",
+    "freezes",
+    "slow",
+    "scales"
+  ],
+  "Baked potatoes": [
+    "slow",
+    "vegetarian"
+  ],
+  "Broccoli, pine nut and kidney bean pasta": [
+    "vegetarian",
+    "quick",
+    "scales"
+  ]
+}
