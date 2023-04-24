ed361761ae38e06aee7135cc45d2732853793972
Kit <kit@tray.io>
2023-04-24 16:54:22 +0100

    Fix some recipes not displaying

--- assets/scripts.js
+++ assets/scripts.js
@@ -91,7 +91,7 @@ const getMealUrl = (source) => {
   if (source.type == 'recibase') {
     return `https://reciba.se/${source.permalink}`;
   } else if (source.type == 'google_drive') {
-    return `https://docs.google.com/document/d/${source.id}/`;
+    return `https://drive.google.com/file/d/${source.link}/view`;
   } else if (source.type == 'online') {
     return source.url;
   } else {
